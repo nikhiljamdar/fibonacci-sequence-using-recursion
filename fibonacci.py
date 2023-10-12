035c15599214f92371fffb47883773bd294a1fe8
Nikhil Jamdar <100251717+nikhiljamdar@users.noreply.github.com>
2023-10-12 18:46:40 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if n<=1:
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
+count=int(input("enter the limit :"))
+if count<=0:
+    print("enter number is greater than 0")
+else:
+    for i in range(count):
+        print(fib(i),end=" ")
